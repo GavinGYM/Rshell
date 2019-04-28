6a819db3017f4cd76cc9bc1ffa90185b1c9b5249
YulinLiang <41889386+PeppaPigei@users.noreply.github.com>
2019-04-27 17:55:05 -0700

    Update prototype.cpp

--- prototype/prototype.cpp
+++ prototype/prototype.cpp
@@ -8,12 +8,25 @@ using namespace std;
 int main(){
 	pid_t pid;
 	pid=fork();
-	char *argv[]={"-a",NULL};
-	char* path = "ls";
-	waitpid(pid,NULL,0);
-	if( pid == 0 ){
-		execvp(path,argv);
+	
+	if ( pid < 0){
+		cout << "fork creates child process error!" << endl;
 		exit(0);
 	}
-	return 0;
+	else if ( pid == 0){
+		cout << "fork creates child process successfully!" << endl;
+		cout << "It's in child process!" << endl;
+		sleep (4);
+		
+		char *argv[]={"-a",NULL};
+		char* path = "ls";
+		execvp(path,argv);
+		exit(0);
+	}	
+	else{
+		waitpid(pid,NULL,0);
+		cout << "child process releases successfully!" << endl;
+		cout << "It's in parent process!" << endl;
+	}
+	exit(0);
 }
