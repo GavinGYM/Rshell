4748f47c950834dcb25adb9b8ebb4b4478582da7
Yiming Guo <49181899+GavinGYM@users.noreply.github.com>
2019-05-10 11:46:14 -0700

    Update connector.cpp

--- src/connector.cpp
+++ src/connector.cpp
@@ -31,36 +31,3 @@ char Connector::GetSign()
 {
 	return this->sign;
 };
-
-bool Semicolon::Operate(bool result)
-{
-	// The "Semicolon" class
-	return true;
-}
-
-bool End::Operate(bool result)
-{
-	return false;
-}
-
-bool Or::Operate(bool result)
-{
-	// The "Or" class
-	if (result) {
-		return false;
-	}
-	else {
-		return true;
-	}
-}
-
-bool And::Operate(bool result)
-{
-	// The "And" class
-	if (result) {
-		return true;
-	}
-	else {
-		return false;
-	}
-}
