661d0615de0d07c3ea0a0f354cfde4cc477ca92c
Yiming Guo <49181899+GavinGYM@users.noreply.github.com>
2019-05-10 11:28:16 -0700

    Update connector.cpp

--- src/connector.cpp
+++ src/connector.cpp
@@ -2,7 +2,34 @@
 
 bool Connector::Operate(bool result)
 {
+	if (this->sign == ';') {
+		return true;
+	}
+	else if (this->sign == '|') {
+		if (result) {
+			return false;
+		}
+		else {
+			return true;
+		}
+	}
+	else if (this->sign == '&') {
+		if (result) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	else if (this->sign == '.') {
+		return false;
+	}
 	return false;
+};
+
+char Connector::GetSign()
+{
+	return this->sign;
 };
 
 bool Semicolon::Operate(bool result)
