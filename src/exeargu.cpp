bda71f9134738d99119305885968a6da214bf84f
Yiming Guo <49181899+GavinGYM@users.noreply.github.com>
2019-05-10 16:22:31 -0700

    Update exeargu.cpp

--- src/exeargu.cpp
+++ src/exeargu.cpp
@@ -17,32 +17,36 @@ bool ExeArgu::Operate()
 		exit(0);
 	}
 	else if (pid == 0) {
-		char *argv[];
-		char* path;
-		
 		if(this->argu == ""){
-		argv[] = { const_cast<char*>(this->exe.c_str()),NULL };
-		path = const_cast<char*>(this->exe.c_str());
+			char *argv[] = { const_cast<char*>(this->exe.c_str()),NULL };
+			char* path = const_cast<char*>(this->exe.c_str());
+			int a = execvp(path, argv);
+			if (a == -1) {
+				perror("execution fails!");
+				return false;
+			}
+			else {
+				return true;
+			}
+			exit(0);	
 		}
 		else{
-		argv[] = { const_cast<char*>(this->exe.c_str()), const_cast<char*>(this->argu.c_str()),NULL };
-		path = const_cast<char*>(this->exe.c_str());
+			char *argv[] = { const_cast<char*>(this->exe.c_str()), const_cast<char*>(this->argu.c_str()),NULL };
+			char* path = const_cast<char*>(this->exe.c_str());
+			int a = execvp(path, argv);
+			if (a == -1) {
+				perror("execution fails!");
+				return false;
+			}
+			else {
+				return true;
+			}
+			exit(0);
 		}
-		
-		int a = execvp(path, argv);
-		if (a == -1) {
-			perror("execution fails!");
-			return false;
-		}
-		else {
-			return true;
-		}
-		exit(0);
 	}
 	else {
 		waitpid(pid, NULL, 0);
 	}
-	//exit(0);
 }
 
 string ExeArgu::getExe()
