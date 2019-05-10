359d90b28c67c6045ba1c48b7fb3391dfbd6a945
YulinLiang <41889386+PeppaPigei@users.noreply.github.com>
2019-05-10 15:06:18 -0700

    Update single_command_tests.sh

--- integration_tests/single_command_tests.sh
+++ integration_tests/single_command_tests.sh
@@ -1,1 +1,18 @@
+#!/bin/sh
 
+INPUTS = ("ls -a" "echo hello" "mkdir test" "echo world" "git status")
+
+for input in "${INPUTS[@]}"
+do
+    echo "./rshell ${input}"
+    output=$(./rshell ${input})
+	result=$(${input})
+    echo "Output: \"${output}\""
+    echo "Result: \"${result}\""
+    if [ "${output}" = "${result}" ]
+    then
+        echo "Test passed"
+    else
+        echo "Test failed"
+    fi
+done
