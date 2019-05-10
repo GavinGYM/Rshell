b8aa46aaddf63fff38dc8366d614620909c18bff
YulinLiang <41889386+PeppaPigei@users.noreply.github.com>
2019-05-10 13:35:08 -0700

    Update exeargu.cpp

--- src/exeargu.cpp
+++ src/exeargu.cpp
@@ -25,12 +25,13 @@ bool ExeArgu::Operate()
 		int a = execvp(path, argv);
 		if (a == -1) {
 			perror("execution fails!");
+			exit(0);
 			return false;
 		}
 		else {
+			exit(0);
 			return true;
 		}
-		exit(0);
 	}
 	else {
 		waitpid(pid, NULL, 0);
