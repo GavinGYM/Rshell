af46039bb7c6370cb7ce93d37e82e010f66c7a2e
Yiming Guo <49181899+GavinGYM@users.noreply.github.com>
2019-05-20 00:45:50 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -15,9 +15,42 @@ int main() {
 		Rshellbase *base = new Rshellbase(input);
 		base->Disintegrate(ea, con, com);
 		
-		bool next = true;
-		int i = 0;
+		//The precedence part
+		int pl = 0;
+		int pr = 0;
 		while (com.at(i)->GetConnector()->GetSign()!='.') {
+			if (ea.at(i)->getExe().at(0) == '(') {
+				ea.setExe();
+				ea.setLeftP(pl);
+				pl++;
+			}
+			if (ea.at(i)->getArgu().at(ea.at(i)->getArgu().size()-1) == ')') {
+				ea.setArgu();
+				ea.setRightP(pl);
+				pr++;
+			}			
+		}
+		bool parenStatus = true;
+		if(pl!=pr){
+			cout << "Your parentheses are wrong, please try to type again.";
+			parenStatus = false;
+		}
+		
+		if(parenStatus){
+			//The execute part
+			bool next = true;
+			int i = 0;
+			while (com.at(i)->GetConnector()->GetSign()!='.') {
+				if (ea.at(i)->getExe() == "exit") {
+					status = false;
+					break;
+				}
+				next = com.at(i)->Operate();
+				i++;
+				if(next == false){
+					i++;
+				}
+			}
 			if (ea.at(i)->getExe() == "exit") {
 				status = false;
 				break;
@@ -27,23 +60,14 @@ int main() {
 			if(next == false){
 				i++;
 			}
+			/*
+			Run the execv, and continue when all work are done
+			status = ???;
+			*/
+
+			//cout << con.at(0)->Operate(true);
+			//cout << true;
 		}
-		if (ea.at(i)->getExe() == "exit") {
-			status = false;
-			break;
-		}
-		next = com.at(i)->Operate();
-		i++;
-		if(next == false){
-			i++;
-		}
-		/*
-		Run the execv, and continue when all work are done
-		status = ???;
-		*/
-		
-		//cout << con.at(0)->Operate(true);
-		//cout << true;
 	}
 	return 0;
 }
